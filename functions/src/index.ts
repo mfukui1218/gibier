3ea97a40cc002f8f9ff2560ae0134eab8b97bd50
mfukui1218 <92801943+mfukui1218@users.noreply.github.com>
2025-12-13 20:02:04 +0900

    re

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as admin from "firebase-admin";
 
-export { checkAllowedEmail, requestAllowEmail } from "./auth";
-export { onNotificationCreate } from "./notifications";
-export { notifyAdmins } from "./notifyAdmins"; // ★これを追加
-
 if (!admin.apps.length) {
   admin.initializeApp();
 }
+
+export { checkAllowedEmail, requestAllowEmail } from "./auth";
+export { onNotificationCreate } from "./notifications";
+export { notifyAdmins } from "./notifyAdmins";
