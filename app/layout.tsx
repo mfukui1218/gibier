e28e865355899fd1a48946ab6f2bd0608908b878
mfukui1218 <92801943+mfukui1218@users.noreply.github.com>
2026-02-23 01:48:44 +0900

    Client JS

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <body>
-        hello
+        {children}
       </body>
     </html>
   );
